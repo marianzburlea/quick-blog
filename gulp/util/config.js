3f341aff7c32df893459699cf616d08d94ee6550
Marian Zburlea <marianzburlea@gmail.com>
2017-04-19 12:20:40 +0100

    Update 2017-04-19T11:20:37.162Z

--- gulp/util/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  fileExpression: {
-    copy: '**/*.{doc,docx,zip,pdf}',
-    font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
-    image: '**/*.{jpg,jpeg,gif,svg,png}'
-  }
-};
-
-export default config;
